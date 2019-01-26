cb76d90fd549a4334a0ea2fb502ef98450258610
Scaler2000 <sebrausi94@gmail.com>
2019-01-27 00:02:07 +0100

    added movement and backup

---
+++ Alfa/Assets/Scripts/Scr_player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Scr_player : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+
+    float hspeed = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        hspeed = Input.GetAxisRaw("Horizontal")*runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(hspeed*Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
